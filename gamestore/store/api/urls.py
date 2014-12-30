d61c3acb116f3a38dea23796201e4bd33abafed4
Granholm Martin <martin.granholm@aalto.fi>
2014-12-30 11:45:35 +0200

    fixed a typo in a comment

--- gamestore/store/api/urls.py
+++ gamestore/store/api/urls.py
@@ -4,7 +4,7 @@ from store.api.views import *
 urlpatterns = patterns('',
     # MG: hopefully these patters are not too confusing... (they should work though)
     #
-    # Basically: dev, name and tagged are all optional and can appear in any order (meaning that there are 6 combinations, thus the size patterns below).
+    # Basically: dev, name and tagged are all optional and can appear in any order (meaning that there are 6 combinations, thus the six patterns below).
     # Arguments of the named groups (?P<groupname>) get passed tp api_view as keyword arguments.
     #
     # Note that this regex expects only ascii characters in game names, dev names and tags (which should be good practice for URLs anyway).
