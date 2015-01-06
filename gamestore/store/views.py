01192f0b4ea2b059fe8256569867d4860974612f
Granholm Martin <martin.granholm@aalto.fi>
2015-01-06 11:45:29 +0200

    updated urls and views to match new version of plan

--- gamestore/store/views.py
+++ gamestore/store/views.py
@@ -23,26 +23,20 @@ def my_view(request):
 def signup_view(request):
     return HttpResponse('Welcome to signup. Not implemented')
     
-def games_view(request):
-    return HttpResponse('Welcome to games. Not implemented')
+def all_games_view(request):
+    return HttpResponse('Welcome to all games. Not implemented')
+    
+def my_games_view(request):
+    return HttpResponse('Welcome to my games. Not implemented')
     
 def play_view(request, game):
     return HttpResponse('''Welcome to play. Not implemented
 
 --DEBUG--
 You are playing: {}'''.format(game), content_type='text/plain')
-    
-def highscore_view(request, game):
-        return HttpResponse('''Welcome to highscore. Not implemented
-
---DEBUG--
-Showing scores for: {}'''.format(game), content_type='text/plain')
-    
+        
 def checkout_view(request):
     return HttpResponse('Welcome to checkou. Not implemented')    
     
 def developer_view(request):
     return HttpResponse('Welcome to developer. Not implemented')
-    
-def devsales_view(request):
-    return HttpResponse('Welcome to developer sales. Not implemented')
