2a37f597facc2e0cd45541db67ca0da7d7c2c0a0
Johan Gädda <johan@Johans-MacBook-Air.local>
2015-01-11 10:57:03 +0200

    Commented out import which was causing ImportError

--- gamestore/store/views.py
+++ gamestore/store/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from datetime import utcnow
+#from datetime import utcnow
 
 from store.models import *
 
